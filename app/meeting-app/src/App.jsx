5ebe289a7f3bba38f277fbc800086d2fe6e5e0cd
Sheel Patel <sheel@letswhirl.com>
2023-04-12 17:27:04 -0400

    scheduler updates

---
+++ app/meeting-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Scheduler from './scheduler/scheduler';
+function App() {
+  return (
+    <div className="App">
+      <Scheduler />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/meeting-app/src/scheduler/scheduler.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import ScheduleSelector from 'react-schedule-selector'
+
+
+
+export default function Scheduler(props) {
+    const [schedule, setSchedule] = useState([])
+
+    const handleChange = newSchedule => {
+        setSchedule({ schedule: newSchedule })
+      }
+
+    return (
+        <div>
+            <h1>Meeting Scheduler</h1>
+            <ScheduleSelector
+                selection={schedule}
+                numDays={5}
+                minTime={8}
+                maxTime={22}
+                hourlyChunks={2}
+                onChange={handleChange}
+            />
+        </div>
+    )
+} 
