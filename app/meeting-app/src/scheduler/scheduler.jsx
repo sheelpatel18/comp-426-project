7a4dd34e496bf45f447e43b465e5198874831cf2
Samuel Osei Kojo <kojosam@email.unc.edu>
2023-04-16 19:47:07 -0400

    styling updates on scheduler

--- app/meeting-app/src/scheduler/scheduler.jsx
+++ app/meeting-app/src/scheduler/scheduler.jsx
@@ -10,11 +10,15 @@ export default function Scheduler(props) {
 
   const handleSubmit = () => {
     console.log(schedule);
-    
   };
 
   const handleReset = () => {
     setSchedule([]);
+  };
+
+  const handleCreate = () => {
+    console.log("Create button clicked!");
+    // Add code to handle creating the meeting here
   };
 
   return (
@@ -26,7 +30,10 @@ export default function Scheduler(props) {
       <h1 style={{ 
         marginBottom: '1rem', 
         fontSize: '2rem', 
-        fontWeight: 'bold' 
+        fontWeight: 'bold',
+        color: '#007bff',
+        textTransform: 'uppercase',
+        letterSpacing: '0.2rem' 
       }}>Meeting Scheduler</h1>
       <ScheduleSelector
         selection={schedule}
@@ -49,12 +56,15 @@ export default function Scheduler(props) {
         <button style={{ 
           border: 'none', 
           borderRadius: '6px', 
-          backgroundColor: '#007bff', 
+          backgroundColor: '#dc3545', 
           color: '#fff', 
           padding: '0.5rem 1rem', 
           fontWeight: 'bold', 
-          cursor: 'pointer' 
-        }} onClick={handleReset}>Reset</button>
+          cursor: 'pointer',
+          transition: 'background-color 0.2s ease-in-out'
+        }} onClick={handleReset} 
+        onMouseEnter={(e) => e.target.style.backgroundColor = '#c82333'}
+        onMouseLeave={(e) => e.target.style.backgroundColor = '#dc3545'}>Reset</button>
         <button style={{ 
           border: 'none', 
           borderRadius: '5px', 
@@ -62,8 +72,23 @@ export default function Scheduler(props) {
           color: '#fff', 
           padding: '0.5rem 1rem', 
           fontWeight: 'bold', 
-          cursor: 'pointer' 
-        }} onClick={handleSubmit}>Submit</button>
+          cursor: 'pointer',
+          transition: 'background-color 0.2s ease-in-out'
+        }} onClick={handleSubmit} 
+        onMouseEnter={(e) => e.target.style.backgroundColor = '#218838'}
+        onMouseLeave={(e) => e.target.style.backgroundColor = '#28a745'}>Submit</button>
+        <button style={{ 
+          border: 'none', 
+          borderRadius: '5px', 
+          backgroundColor: '#007bff', 
+          color: '#fff', 
+          padding: '0.5rem 1rem', 
+          fontWeight: 'bold', 
+          cursor: 'pointer',
+          transition: 'background-color 0.2s ease-in-out'
+        }} onClick={handleCreate} 
+        onMouseEnter={(e) => e.target.style.backgroundColor = '#0069d9'}
+        onMouseLeave={(e) => e.target.style.backgroundColor = '#007bff'}>Create</button>
       </div>
     </div>
   );
