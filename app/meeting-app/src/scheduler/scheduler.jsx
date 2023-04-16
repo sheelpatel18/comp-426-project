65c27679aaca066ddecbcf54784cb96f4d846819
Vidyuth Jagadeesan <vidyuthj@email.unc.edu>
2023-04-16 16:32:45 -0400

    latest

--- app/meeting-app/src/scheduler/scheduler.jsx
+++ app/meeting-app/src/scheduler/scheduler.jsx
@@ -1,26 +1,70 @@
-import React, { useState } from 'react'
-import ScheduleSelector from 'react-schedule-selector'
-
-
+import React, { useState } from 'react';
+import ScheduleSelector from 'react-schedule-selector';
 
 export default function Scheduler(props) {
-    const [schedule, setSchedule] = useState([])
+  const [schedule, setSchedule] = useState([]);
 
-    const handleChange = newSchedule => {
-        setSchedule({ schedule: newSchedule })
-      }
+  const handleChange = (newSchedule) => {
+    setSchedule(newSchedule);
+  };
 
-    return (
-        <div>
-            <h1>Meeting Scheduler</h1>
-            <ScheduleSelector
-                selection={schedule}
-                numDays={5}
-                minTime={8}
-                maxTime={22}
-                hourlyChunks={2}
-                onChange={handleChange}
-            />
-        </div>
-    )
-} 
+  const handleSubmit = () => {
+    console.log(schedule);
+    
+  };
+
+  const handleReset = () => {
+    setSchedule([]);
+  };
+
+  return (
+    <div style={{ 
+      border: '1px solid #ccc', 
+      borderRadius: '5px', 
+      padding: '1rem' 
+    }}>
+      <h1 style={{ 
+        marginBottom: '1rem', 
+        fontSize: '2rem', 
+        fontWeight: 'bold' 
+      }}>Meeting Scheduler</h1>
+      <ScheduleSelector
+        selection={schedule}
+        numDays={7}
+        minTime={10}
+        maxTime={22}
+        hourlyChunks={1}
+        onChange={handleChange}
+        style={{ 
+          border: '1px solid #ccc', 
+          borderRadius: '5px', 
+          padding: '1rem' 
+        }}
+      />
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        marginTop: '1rem' 
+      }}>
+        <button style={{ 
+          border: 'none', 
+          borderRadius: '5px', 
+          backgroundColor: '#007bff', 
+          color: '#fff', 
+          padding: '0.5rem 1rem', 
+          fontWeight: 'bold', 
+          cursor: 'pointer' 
+        }} onClick={handleReset}>Reset</button>
+        <button style={{ 
+          border: 'none', 
+          borderRadius: '5px', 
+          backgroundColor: '#28a745', 
+          color: '#fff', 
+          padding: '0.5rem 1rem', 
+          fontWeight: 'bold', 
+          cursor: 'pointer' 
+        }} onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
+  );
+}
