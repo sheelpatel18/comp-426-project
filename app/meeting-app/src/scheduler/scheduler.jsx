08172919583d6b1f946ccae9af3021a5a2adf28e
Vidyuth Jagadeesan <vidyuthj@email.unc.edu>
2023-05-06 21:06:02 -0400

    added comments to scheduler.jsx

--- app/meeting-app/src/scheduler/scheduler.jsx
+++ app/meeting-app/src/scheduler/scheduler.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import ScheduleSelector from 'react-schedule-selector'
-
+//This is where the implementation of the scheduler takes place. 
 
 
 export default function Scheduler(props) {
+    // 
     const [schedule, setSchedule] = useState([])
-
+    // This handles the state changes when certains times are selected.
     const handleChange = newSchedule => {
         setSchedule({ schedule: newSchedule })
       }
 
     return (
-        <div>
+       //Title of the Meeting Scheduler with the numbers of days, the time for each day and how many blocks exist for each time. This is what is returned and the scheduler is instantiated.
+       <div>
             <h1>Meeting Scheduler</h1>
             <ScheduleSelector
                 selection={schedule}
